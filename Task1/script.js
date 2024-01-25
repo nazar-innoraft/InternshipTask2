f7d3092782167baeddefe001023f85fd7f47d35c
nazar-innoraft <nazar.ali@innoraft.com>
2024-01-25 14:42:01 +0530

    animation and js for sliding added

--- Task1/script.js
+++ Task1/script.js
@@ -4,23 +4,24 @@ const links = ["img/backgruound1.jpeg", "img/backgruound2.jpeg", "img/backgruoun
 const cities = ["Kolkata, India", "Bangalore, India", "Puna, India", "Chennai, India", "Indore, India"];
 const names = ["Axis Mall", "Central Park", "Victoria", "Diamond Plazza", "Central One"];
 const desc = ["Axis Mall Axis Mall Axis Mall", "Central Park Central Park Central Park", "Victoria Victoria Victoria", "Diamond PlazzaDiamond PlazzaDiamond Plazza", "Central OneCentral OneCentral One"];
-setInterval(function () {
+function changeElement() {
     i = i % 5;
     document.getElementById("stripimg").src = links[i];
     document.querySelector(".right").style.backgroundImage = `url("${links[i]}")`;
     document.querySelector(".city").innerHTML = cities[i];
     document.querySelector(".name").innerHTML = names[i];
     document.querySelector(".des").innerHTML = desc[i];
-    document.querySelector(".text").animate([
+    document.querySelector(".text").animate(
         // key frames
         {
             transform: ['translateY(100px)', 'translateY(0px)'],
-            opacity: [0, 1]
-        }
-    ], {
+            opacity: [0,1]
+        },
+     {
         // sync options
         duration: 600
     });
 
     i++;
-}, 3000);
+}
+setInterval(changeElement, 3000);
